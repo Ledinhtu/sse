61916797d205c98cdd2e7d8a0a8ab0146cbad1b6
Ledinhtu <noinayconhau@gmail.com>
2023-02-13 06:36:54 +0700

    ignore

--- config/db/db_config.js
+++ config/db/db_config.js
@@ -1,26 +1,21 @@
-const { initializeApp } = require("firebase/app");
-const { getDatabase} = require("firebase/database");
+  const { initializeApp } = require("firebase/app");
+  const { getDatabase} = require("firebase/database");
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  // ...
-    apiKey: "AIzaSyBJU-RR2MKhO9SgWaQMAwa2A2yU6_QKwLc",
-    authDomain: "iot-datastream-tu2142.firebaseapp.com",
-    // databaseURL: "https://iot-datastream-tu2142-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "iot-datastream-tu2142",
-    storageBucket: "iot-datastream-tu2142.appspot.com",
-    messagingSenderId: "231851759724",
-    appId: "1:231851759724:web:0273a32b2d1246d44f6049",
-  // The value of `databaseURL` depends on the location of the database
-    databaseURL: "https://iot-datastream-tu2142-default-rtdb.asia-southeast1.firebasedatabase.app/",
-};
+  const firebaseConfig = {
+      apiKey: "AIzaSyBJU-RR2MKhO9SgWaQMAwa2A2yU6_QKwLc",
+      authDomain: "iot-datastream-tu2142.firebaseapp.com",
+      projectId: "iot-datastream-tu2142",
+      storageBucket: "iot-datastream-tu2142.appspot.com",
+      messagingSenderId: "231851759724",
+      appId: "1:231851759724:web:0273a32b2d1246d44f6049",
+      databaseURL: "https://iot-datastream-tu2142-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+  // Initialize Realtime Database and get a reference to the service
+  const database = getDatabase(app);
 
-module.exports = database;
+  module.exports = database;
